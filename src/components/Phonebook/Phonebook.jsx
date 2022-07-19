582f77a701c4e0a41b3bdf753d9146e4b51857ed
Max_Hrytsenia <95626499+MaxymH@users.noreply.github.com>
2022-07-19 20:10:41 +0200

    finish

---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,87 @@
+
+import { useEffect, useState } from "react"
+import { nanoid } from 'nanoid';
+
+import Filter from "./Filter";
+import TitlePhonebook from "./TitlePhonebook";
+import Form from "./Form"
+import ContactList from "./ContactList";
+
+const Phonebook = () => {
+    const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('contacts')))
+    const [filter, setFilter] = useState('')
+
+    useEffect(() => {
+    const contact = window.localStorage.getItem('contacts')
+    const parseContacts = JSON.parse(contact)
+        if (localStorage.getItem('contacts')) {
+            setContacts(parseContacts)
+        }
+    }, [])
+            
+    useEffect(() => {
+    
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+    }, [contacts])
+    
+    const addContact = ({ name, number }) => {
+        if (contacts.find(item => item.name === name)) {
+            alert(`${name} is already in your Phonebook`);
+            return;
+            }
+
+        setContacts(prevState => {
+            const newContact = {
+            id: nanoid(),
+            name,
+            number,
+        };
+        return[...prevState, newContact];
+        })
+        
+    }
+    const filterr = e => {
+        setFilter(e.target.value)
+    }
+
+    const filteredContact = () => {
+    if (!filter) {
+            return contacts;
+        }
+        const filteredContacts = contacts.filter(({ name }) => {
+            const res = name.toLowerCase().includes(filter.toLowerCase());
+            return res;
+        });
+        
+        return filteredContacts;
+    }
+
+    const deleted = id => {
+        
+        const res = contacts.filter(contact => contact.id !== id)
+        setContacts(res)
+    }
+
+    return ( 
+            <div>
+                <TitlePhonebook title='Phonebook'/>
+            <Form
+                onSubmit={addContact}
+            />
+            <TitlePhonebook
+                title='Contacts'
+            />
+            <Filter
+                onChange={filterr}
+                filter={filter}
+            />
+                <ContactList
+                    contacts={filteredContact()}
+                    deleted={deleted}
+                />
+            </div>
+        )
+}
+
+
+export default Phonebook
